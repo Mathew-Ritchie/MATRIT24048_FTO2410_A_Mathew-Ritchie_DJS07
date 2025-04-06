c2d0ef2c22ecd13f7419e08a5873d5aaa45ec899
Mathew Ritchie <ritchmat89@gmail.com>
2025-04-06 12:55:43 +0200

    changed header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
   return (
     <header className="header">
       <img src={puppyImage} className="header--image" />
-      <h2 className="header--title">Header</h2>
+      <h2 className="header--title">Meme Generator!</h2>
       <h4 className="header--project">CodeSpace - DJS07</h4>
     </header>
   );
